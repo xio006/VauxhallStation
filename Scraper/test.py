a8b5b621172dba53773dee5c4e76d573886a598c
xio006 <x.xiong01@gmail.com>
2017-03-27 21:48:05 +0100

    test commit

--- Scraper/test.py
+++ Scraper/test.py
@@ -3,7 +3,7 @@ import lxml
 from lxml import html
 import xml
 import requests
-
+import numpy
 
 
 url = 'http://www.argos.co.uk/static/Browse/ID72/33015263/c_1/1|category_root|Home+and+garden|33005908/c_2/2|33005908|Living+room+furniture|33008988/c_3/3|cat_33008988|Armchairs+and+chairs|33015263.htm'
