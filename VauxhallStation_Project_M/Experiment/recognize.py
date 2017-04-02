e5222bc99238cbcc8db2bae9fcff45225a8207a4
xio006 <x.xiong01@gmail.com>
2017-04-02 21:55:40 +0100

    added contour recognition

--- VauxhallStation_Project_M/Experiment/recognize.py
+++ VauxhallStation_Project_M/Experiment/recognize.py
@@ -5,26 +5,32 @@ import sys
 import glob
 import multiprocessing
 
-
-_src = cv.imread("./standard/circles-eight.jpg")
-eight_pancakes = cv.resize(_src,(0,0),fx = 0.2, fy = 0.2)
-
-_src = cv.imread("./demo-images/multiple3-cropped.jpg")
-chain_tiles_1 = cv.resize(_src,(0,0),fx=0.5,fy=0.5)
-
-
 def display_img(img):
+    """Display image and wait for enter key to close the window"""
     cv.imshow("Display Window",img)
     cv.waitKey(0)
 
-#filters.recolorRC(chain_tiles_1,chain_tiles_1)
-#filters.recolorRGV(chain_tiles_1,chain_tiles_1)
-#filters.recolorCMV(chain_tiles_1,chain_tiles_1)
-#CurveFilter = filters.BGRPortraCurveFilter()
-#CurveFilter.apply(chain_tiles_1,chain_tiles_1)
-#filters.strokeEdges(chain_tiles_1,chain_tiles_1,3,5)
+def read_and_resize(img_path,resize_perc):
+    """read image from image_path and resize proportionally by resize_perc
+    resize perc takes a decimal number"""
+    _src = cv.imread(img_path)
+    res = cv.resize(_src,(0,0),fx=resize_perc,fy=resize_perc)
+    return res
 
+eight_pancakes = read_and_resize("./pic/standard/circles-eight.jpg",0.2)
+chain_tiles_1 = read_and_resize("./pic/demo-images/multiple3.jpg",0.2)
 
+gray = cv.cvtColor(chain_tiles_1, cv.COLOR_RGB2GRAY)
+#gray = cv.bilateralFilter(gray, 11, 17, 17)
+gray = cv.GaussianBlur(gray, (5, 5), 0)
+_res = None
+#threshold = cv.adaptiveThreshold(gray, 255, 1, 1, 11, 2)
+edged = cv.Canny(gray, 10, 30, _res, 3, True)
 
+display_img(edged)
+(_,cnts, _) = cv.findContours(edged.copy(), mode = cv.RETR_LIST,  method = cv.CHAIN_APPROX_TC89_L1)
+cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:10]
 
+cv.drawContours(chain_tiles_1,cnts,-1,(0,255,0),3)
 display_img(chain_tiles_1)
+
